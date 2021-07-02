90cff4a28b851d71250d6f07979aab1902557bbc
Joel Flores <joelf.-@live.com>
2021-07-02 00:14:51 -0300

    add search by queries, pagination and limit

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -3,16 +3,45 @@ const Category = require('../models/Category')
 const { Op } = require('sequelize')
 
 exports.getProducts = async (req, res) => {
-  const products = await Product.findAndCountAll({
-    include: [Category],
-    attributes: { exclude: 'category' },
-    offset: 0,
-    limit: 5,
-    where: {
-      name: {
-        [Op.substring]: req.query.search
-      }
-    },
-  })
-  res.json(products)
+  const query = {}
+
+  if(req.query) {
+    const { search, limit, page } = req.query
+    
+    if(search) query.where = { name: { [Op.substring]: search } }
+
+    if(limit) query.limit = parseInt(limit)
+
+    if(page) query.offset = parseInt(page) > 0 ? (parseInt(page) - 1) * parseInt(limit) : 0
+  }
+
+  try {
+    const products = await Product.findAndCountAll({
+      include: [Category],
+      attributes: { exclude: 'category' },
+      ...query,
+    })
+    
+    if(!products.count) {
+      res
+        .status(404)
+        .send({
+          body: 'products not found'
+        })
+    }
+
+    res
+      .status(200)
+      .send({
+        body: products
+      })
+      
+  } catch (error) {
+    res
+      .status(500)
+      .send({
+        body: 'internal error'
+      })
+  }
+  
 }
